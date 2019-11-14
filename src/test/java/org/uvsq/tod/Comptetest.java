b842dfe23cd51eff33cb7f92fe6b18d8387f582a
kabache <nassim199206@gmail.com>
2019-11-14 14:48:03 +0100

    versionfinal

--- src/test/java/org/uvsq/tod/Comptetest.java
+++ src/test/java/org/uvsq/tod/Comptetest.java
@@ -10,14 +10,16 @@ public class Comptetest {
 	public void test() {
 		Compte c= new Compte(500);
 		c.debiteSolde(200);
-		assertTrue("debite refusé",c.solde==300);
+		assertTrue("debite refusï¿½",c.solde==300);
 	}
+	
+	
 	@Test
 	public void teest1() {
 		Compte c= new Compte(500);
 		Compte c1=new Compte(1000);
 		c.virement(500, c1);
-		assertTrue("debité refusé",c1.solde==1000); 
+		assertTrue("debitï¿½ refusï¿½",c1.solde==1000); 
 		
 	}
 	
@@ -25,7 +27,7 @@ public class Comptetest {
 	public void test2() {
 		Compte c= new Compte(500);
 		c.crediteSolde(200);
-		assertTrue("debite refusé",c.solde==700);
+		assertTrue("debite refusï¿½",c.solde==700);
 	}
 	
 	
